3ffc7392cbc1f130f910f5ec66a9318dd80fc7ff
locletvl842002 <loc.letvl@hcmut.edu.vn>
2022-06-17 18:48:42 +0700

    change color lualine

--- lua/tokyo-night/NeoTree.lua
+++ lua/tokyo-night/NeoTree.lua
@@ -1,11 +1,11 @@
 local NeoTree = {
-	NeoTreeNormal = { fg = C.dir_name, bg = C.dark },
+	NeoTreeNormal = { fg = C.fg, bg = C.dark },
 	NeoTreeNormalNC = { fg = C.fg, bg = C.dark },
 	-- NeoTreeNormalNC = { fg = C.light_gray, bg = C.bg },
 	NeoTreeSignColumn = { fg = C.fg, bg = C.dark },
 	NeoTreeEndOfBuffer = { fg = C.dark, bg = C.dark },
 	NeoTreeStatusLine = { fg = C.dark, bg = C.dark },
-	NeoTreeCursorLine = { fg = C.fg, bg = C.cursor_line },
+	NeoTreeCursorLine = { bg = C.cursor_line },
 	-- NeoTreeWinSeparator = { fg = C.bg, bg = C.bg },
 	NeoTreeWinSeparator = { fg = C.bg, bg = C.bg },
 	NeoTreeRootName = { style = "bold" },
